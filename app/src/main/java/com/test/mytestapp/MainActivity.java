c4e4ebb55d5349a8ab444ff850985dd343954703
Marta <martawitkowska123@gmail.com>
2018-01-30 00:12:45 +0100

    This is my second commit - maybe this one will work...

--- app/src/main/java/com/test/mytestapp/MainActivity.java
+++ app/src/main/java/com/test/mytestapp/MainActivity.java
@@ -15,14 +15,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+//        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+//        setSupportActionBar(toolbar);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "This is my action", Snackbar.LENGTH_LONG)
+                Snackbar.make(view, "This is my new action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
         });
